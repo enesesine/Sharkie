dff45a2ab5b402aad51a8efcf480f9a390530b7c
enesesine <eneslucker@gmail.com>
2025-01-25 03:24:47 +0100

    js

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -1,9 +1,11 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MoveableObject();
 
 function init() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
   character.src = "./Imgs/1.Sharkie/1.IDLE/1.png";
+
+  console.log("My Character is", character);
 }
