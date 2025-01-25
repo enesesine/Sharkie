a98ea05563f715c4348616eceef8190270806a22
enesesine <eneslucker@gmail.com>
2025-01-25 04:43:33 +0100

    error image need fix

--- models/world.class.js
+++ models/world.class.js
@@ -2,5 +2,20 @@ class World {
   character = new Character();
   enemies = [new Fish(), new Fish(), new Fish()];
 
-  draw() {}
+  ctx;
+
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.draw();
+  }
+
+  draw() {
+    this.ctx.drawImage(
+      this.character.image,
+      this.character.x,
+      this.character.y,
+      this.character.width,
+      this.character.height
+    );
+  }
 }
