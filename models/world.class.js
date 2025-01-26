5af3bd5dafbf08338e5f5da1959dd1a147b6b13d
enesesine <eneslucker@gmail.com>
2025-01-26 02:51:09 +0100

    AnimationFrame

--- models/world.class.js
+++ models/world.class.js
@@ -1,21 +1,29 @@
 class World {
   character = new Character();
   enemies = [new Fish(), new Fish(), new Fish()];
-
+  canvas;
   ctx;
 
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
     this.draw();
   }
 
   draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
     this.ctx.drawImage(
-      this.character.image,
+      this.character.img,
       this.character.x,
       this.character.y,
       this.character.width,
       this.character.height
     );
+
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
   }
 }
