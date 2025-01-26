18f728655e1f99e7299930767dd060fd52cafc7b
enesesine <eneslucker@gmail.com>
2025-01-26 03:08:01 +0100

    enemies

--- models/world.class.js
+++ models/world.class.js
@@ -20,6 +20,15 @@ class World {
       this.character.width,
       this.character.height
     );
+    this.enemies.forEach((enemy) => {
+      this.ctx.drawImage(
+        enemy.img,
+        enemy.x,
+        enemy.y,
+        enemy.width,
+        enemy.height
+      );
+    });
 
     let self = this;
     requestAnimationFrame(function () {
