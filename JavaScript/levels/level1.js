7528f5edc12f303ec813ea6d77894f25eaf2ae22
enesesine <eneslucker@gmail.com>
2025-01-28 03:28:10 +0100

    levels

---
+++ JavaScript/levels/level1.js
@@ -0,0 +1,83 @@
+const level1 = new Level(
+  [new Fish(), new Fish(), new Fish()],
+
+  [
+    new BackgroundObject(
+      "  Imgs/3. Background/Layers/5. Water/D2.png",
+      -720 * 2
+    ),
+
+    new BackgroundObject(
+      "  Imgs/3. Background/Legacy/Layers/3.Fondo 1/D1.png",
+      -720 * 2
+    ),
+
+    new BackgroundObject(
+      "Imgs/3. Background/Layers/4.Fondo 2/D1.png",
+      -720 * 2
+    ),
+
+    new BackgroundObject("Imgs/3. Background/Layers/2. Floor/D1.png", -720 * 2),
+
+    new BackgroundObject("  Imgs/3. Background/Layers/5. Water/D1.png", -720),
+
+    new BackgroundObject(
+      "  Imgs/3. Background/Legacy/Layers/3.Fondo 1/D2.png",
+      -720
+    ),
+
+    new BackgroundObject("Imgs/3. Background/Layers/4.Fondo 2/D2.png", -720),
+
+    new BackgroundObject("Imgs/3. Background/Layers/2. Floor/D2.png", -720),
+
+    new BackgroundObject("  Imgs/3. Background/Layers/5. Water/D2.png", 0),
+
+    new BackgroundObject(
+      "  Imgs/3. Background/Legacy/Layers/3.Fondo 1/D1.png",
+      0
+    ),
+
+    new BackgroundObject("Imgs/3. Background/Layers/4.Fondo 2/D1.png", 0),
+
+    new BackgroundObject("Imgs/3. Background/Layers/2. Floor/D1.png", 0),
+
+    new BackgroundObject("  Imgs/3. Background/Layers/5. Water/D1.png", 720),
+
+    new BackgroundObject(
+      "  Imgs/3. Background/Legacy/Layers/3.Fondo 1/D2.png",
+      720
+    ),
+
+    new BackgroundObject("Imgs/3. Background/Layers/4.Fondo 2/D2.png", 720),
+
+    new BackgroundObject("Imgs/3. Background/Layers/2. Floor/D2.png", 720),
+
+    new BackgroundObject(
+      "  Imgs/3. Background/Layers/5. Water/D2.png",
+      720 * 2
+    ),
+
+    new BackgroundObject(
+      "  Imgs/3. Background/Legacy/Layers/3.Fondo 1/D1.png",
+      720 * 2
+    ),
+
+    new BackgroundObject("Imgs/3. Background/Layers/4.Fondo 2/D1.png", 720 * 2),
+
+    new BackgroundObject("Imgs/3. Background/Layers/2. Floor/D1.png", 720 * 2),
+
+    new BackgroundObject(
+      "  Imgs/3. Background/Layers/5. Water/D1.png",
+      720 * 3
+    ),
+
+    new BackgroundObject(
+      "  Imgs/3. Background/Legacy/Layers/3.Fondo 1/D2.png",
+      720 * 3
+    ),
+
+    new BackgroundObject("Imgs/3. Background/Layers/4.Fondo 2/D2.png", 720 * 3),
+
+    new BackgroundObject("Imgs/3. Background/Layers/2. Floor/D2.png", 720 * 3),
+  ]
+);
---
+++ models/levels.class.js
@@ -0,0 +1,11 @@
+class Level {
+  enemies;
+  surfaces;
+  backgroundObjects;
+
+  constructor(enemies, backgroundObjects) {
+    this.enemies = enemies;
+    this.clouds = this.clouds;
+    this.backgroundObjects = backgroundObjects;
+  }
+}
