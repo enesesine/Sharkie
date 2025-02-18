dcec99041e0b7d7981f44b70e7d07ad2da654d53
enesesine <eneslucker@gmail.com>
2025-02-18 02:54:02 +0100

    code fix comments

--- JavaScript/levels/restart-home-game.js
+++ JavaScript/levels/restart-home-game.js
@@ -1,7 +1,7 @@
 function homeGame() {
   clearAllGameIntervals();
   document.getElementById("game-over-screen").style.display = "none";
-  document.getElementById("win-screen").style.display = "none"; // Hier den Win-Screen ausblenden
+  document.getElementById("win-screen").style.display = "none";
   document.getElementById("game-container").style.display = "none";
   document.getElementById("startscreen").style.display = "flex";
 }
